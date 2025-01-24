e319a1a2dbe36883d7d2fdfdb8ccd606480c8161
realsahabia <yakubukarim12@gmail.com>
2025-01-24 00:09:29 +0000

    added frontend folder

---
+++ raffle-frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Header from "./components/Header";
+
+export default function Home() {
+  return (
+    <div className="bg-[#091B18] min-h-screen flex flex-col">
+      <Header />
+    </div>
+  );
+}
